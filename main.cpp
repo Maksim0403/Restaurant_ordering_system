3690f4b2e843c5cbbbf5cd618330204bfcd7abc6
Maksim0403 <maksim.didychuk4@gmail.com>
2024-02-21 21:29:20 +0200

    Initial project.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+plan:
+ theme: restaurant ordering system
+ 1. class restaurant (title, location, status)
+ 2. class employee (name, age, position, salary)
+ 3. class order (items, price)
+*/
+
+int main() {
+    cout << "Hello world" << endl;
+    return 0;
+}
