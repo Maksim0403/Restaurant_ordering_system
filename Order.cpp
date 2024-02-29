2d3735e44fb9d0e095bb4791619590af437cb249
Maksim0403 <maksim.didychuk4@gmail.com>
2024-02-29 18:34:50 +0200

    Lab 3. Changes in Order.

--- Order.cpp
+++ Order.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+
 string &Order::newItem() {
     return items;
 }
@@ -9,17 +10,75 @@ string &Order::newItem() {
 int &Order::priceOfItem() {
     return price;
 }
+//const
+void Order::setItems(const string &items) {
+    this->items = items;
+}
+
+void Order::setPrice(int price) {
+    this->price = price;
+}
+//overloading binary operator
+Order Order::operator+(Order order2) {
+    Order order;
+    order.price = price + order2.price;
+    order.items = items + order2.items;
+    return order;
+}
+
+int Order::discount(int newPrice) {
+    if(price >= 300){
+        price -= newPrice;
+    }
+    return price;
+}
+
+std::istream &operator>>(istream &is, Order &order) {
+    cout<<"Enter item: "<<endl;
+    is>>order.items;
+    cout<<"Enter price: "<<endl;
+    is>>order.price;
+    return is;
+}
+
+std::ostream &operator<<(ostream &os, Order &order) {
+    os << "Item: "<< order.items <<endl<<
+       "Price: "<< order.price <<endl<<
+       "-------------------------------------"<<endl;
+    return os;
+}
+
+Order::Order() {
+    items = "";
+    price = 0;
+    cout<<"Constructor with no args was called"<<endl;
+}
 
 Order::Order(string items, int price)
-        : items(items), price(price) {}
+        : items(items), price(price) {cout<<"Constructor was called"<<endl;}
+
+
+Order::Order(const Order &other)
+        :items(other.items), price(other.price) {cout<<"Copy constructor was called"<<endl;}
+
 
 Order::~Order() {}
 
+
 void Order::outputOrderInfo() {
+    Order::discount(50);
     cout<<"Item: "<< items <<endl<<
         "Price: "<<price<<endl;
         cout<<"-------------------------------------"<<endl;
 }
+
+
+
+
+
+
+
+
 
 
 
--- Order.h
+++ Order.h
@@ -10,12 +10,23 @@ private:
     string items;
     int price;
 public:
+    //overloading friend operators
+    friend std::istream& operator>> (std::istream &is, Order &order);
+    friend std::ostream& operator<< (std::ostream &os, Order &order);
+
+    Order();
+    Order(const Order &other);
     Order(string items, int price);
+    Order operator+(Order order2);
     ~Order();
 
+    int discount(int newPrice);
+    void setItems(const string &items);
+    void setPrice(int price);
     string &newItem();
     int &priceOfItem();
     void outputOrderInfo();
+
 };
 
 
