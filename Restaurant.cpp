ef8b55b5c328d9560f94c5d08ccb96a245cd6929
Maksim0403 <maksim.didychuk4@gmail.com>
2024-02-29 18:35:30 +0200

    Lab 3. Changes in Restaurant.

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -5,20 +5,48 @@ string &Restaurant::getStatus() {
     return status;
 }
 
-string &Restaurant::getTitle() {
+string Restaurant::getTitle() const {
     return title;
 }
 
-string &Restaurant::getLocation() {
+string Restaurant::getLocation() const {
     return location;
 }
 
+void Restaurant::setTitle(const string &title) {
+    this->title = title;
+}
+
+void Restaurant::setLocation(const string &location) {
+    this->location = location;
+}
+
+void Restaurant::setStatus(string &status) {
+    Restaurant::status = status;
+}
+
+ostream &operator<<(ostream &os, const Restaurant &restaurant) {
+    os << "Title: "<<restaurant.title<<endl<<
+        "Location: "<<restaurant.location<<endl<<
+        "Status: "<<restaurant.status<<endl<<
+        "-------------------------------------"<<endl;
+    return os;
+}
+
+Restaurant::Restaurant() {
+        cout<<"Constructor with no args was called"<<endl;
+}
+
 Restaurant::Restaurant(string title, string location, string status)
-        : title{title}, location{location}, status{status} {}
+        : title{title}, location{location}, status{status} {cout<<"Constructor was called"<<endl;}
+
+Restaurant::Restaurant(const Restaurant &other)
+        :title(other.title), location(other.location), status(other.status) {cout<<"Copy constructor was called"<<endl;}
 
 Restaurant::~Restaurant() {
-
+    cout<<"Destructor was called"<<endl;
 }
+
 
 void Restaurant::outputRestaurantInfo() {
     cout<<"Title: "<<title<<endl<<
@@ -26,6 +54,9 @@ void Restaurant::outputRestaurantInfo() {
         "Status: "<<status<<endl;
     cout<<"-------------------------------------"<<endl;
 }
+
+
+
 
 
 
--- Restaurant.h
+++ Restaurant.h
@@ -10,12 +10,22 @@ private:
     string location;
     string status;
 public:
+    //overloading friend operators
+    friend std::ostream& operator<< (std::ostream &os, const Restaurant &restaurant);
+
+    Restaurant();
     Restaurant(string newTitle = "None", string newLocation = "None", string status = "Undefined");
+    Restaurant(const Restaurant &other);
     virtual ~Restaurant();
 
-    string &getTitle();
-    string &getLocation();
+    string getTitle() const;
+    string getLocation() const;
     string &getStatus();
+
+    void setTitle(const string &title);
+    void setLocation(const string &location);
+    void setStatus(string &status);
+
     void outputRestaurantInfo();
 };
 
