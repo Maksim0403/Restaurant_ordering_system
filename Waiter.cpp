8be0d08c4e26bc79a9032cd8121fd4dae1045893
Maksim0403 <maksim.didychuk4@gmail.com>
2024-03-19 11:57:00 +0200

    Lab 5. Add virtual functions to the class Waiter

--- Waiter.cpp
+++ Waiter.cpp
@@ -4,24 +4,13 @@ int Waiter::getFloorNumber() const {
     return floorNumber;
 }
 
-Waiter::Waiter() {}
+Order Waiter::getOrder() const {
+    return order;
+}
 
-Waiter::Waiter(string name, int age)
-        : Employee(name, age) {cout<<"Derived constructor"<<endl;}
-
-Waiter::Waiter(string name, int age, string position, int salary)
-        : Employee(name, age, position, salary) {cout<<"Derived constructor"<<endl;}
-
-Waiter::Waiter(string name, int age, string position, int salary, int floorNumber, Order order)
-        : Employee(name, age, position, salary), floorNumber{floorNumber}, order{order} {cout<<"Derived constructor"<<endl;}
-
-Waiter::~Waiter() {cout << "Derived destructor" << endl;}
-
-Waiter::Waiter(const Waiter &other)
-        : Employee(other), floorNumber{other.floorNumber} {cout << "Derived copy constructor" << endl;}
-
-Waiter::Waiter(Waiter &&other) noexcept
-        : Employee(other), floorNumber{other.floorNumber} {cout << "Derived move constructor" << endl;}
+void Waiter::setFloorNumber(int floorNumber) {
+    Waiter::floorNumber = floorNumber;
+}
 
 Waiter &Waiter::operator=(const Waiter &rhs) {
     if(this != &rhs){
@@ -31,14 +20,61 @@ Waiter &Waiter::operator=(const Waiter &rhs) {
     return *this;
 }
 
-ostream &operator<<(ostream &os, Waiter &waiter) {
+ostream &operator<<(ostream &os, Waiter &waiter)  {
     os <<"Name: "<<waiter.name<<endl<<
-           "Age: "<<waiter.age<<endl<<
-           "Position: "<<waiter.position<<endl<<
-           "Salary: "<<waiter.salary<<endl<<
-           "Floor number: " << waiter.floorNumber << endl<<
-           "Order: " << waiter.order<<endl;
+       "Age: "<<waiter.age<<endl<<
+       "Position: "<<waiter.position<<endl<<
+       "Salary: "<<waiter.salary<<endl<<
+       "Floor number: " << waiter.floorNumber << endl<<
+       "Order: " << waiter.order<<endl;
     cout<<"-------------------------------------"<<endl;
     return os;
 }
+
+void Waiter::output(ostream &os) const {
+    Employee::output(os);
+    cout<<"Floor number: "<<floorNumber<<endl <<
+        "Order: " << order <<endl;
+}
+
+
+Waiter::Waiter() {amountOfWaiters++;}
+
+Waiter::Waiter(string name, int age)
+        : Employee(name, age) {cout<<"Derived constructor"<<endl;amountOfWaiters++;}
+
+Waiter::Waiter(string name, int age, string position, int salary)
+        : Employee(name, age, position, salary) {cout<<"Derived constructor"<<endl;amountOfWaiters++;}
+
+Waiter::Waiter(string name, int age, string position, int salary, int floorNumber)
+        : Employee(name, age, position, salary), floorNumber{0} {cout<<"Derived constructor"<<endl;amountOfWaiters++;}
+
+Waiter::Waiter(string name, int age, string position, int salary, int floorNumber, Order order)
+        : Employee(name, age, position, salary), floorNumber{floorNumber}, order{order} {cout<<"Derived constructor"<<endl;amountOfWaiters++;}
+
+Waiter::~Waiter() {cout << "Derived destructor" << endl;}
+
+Waiter::Waiter(const Waiter &other)
+        : Employee(other), floorNumber{other.floorNumber} {cout << "Derived copy constructor" << endl;}
+
+Waiter::Waiter(Waiter &&other) noexcept
+        : Employee(other), floorNumber{other.floorNumber} {cout << "Derived move constructor" << endl;}
+
+
+
+int Waiter::amountOfWaiters = 0;
+
+int Waiter::getAmountOfWaiters() {
+    return amountOfWaiters;
+}
+
+void Waiter::outputAmountOfEmployees() const {
+    cout<<"Amount of waiters: "<<Waiter::getAmountOfWaiters()<<endl;
+}
+
+
+
+
+
+
 
--- Waiter.h
+++ Waiter.h
@@ -8,26 +8,34 @@ using namespace std;
 #define LAB2_CLASS_OBJECTS_WAITER_H
 
 
-class Waiter : public Employee{
-private:
+class Waiter : public Employee {
+protected:
+
     int floorNumber;
     Order order;
+    static int amountOfWaiters;
 public:
-
     friend ostream &operator<<(ostream &os, Waiter &waiter);
 
     Waiter();
     Waiter(string name, int age);
     Waiter(string name, int age, string position, int salary);
+    Waiter(string name, int age, string position, int salary, int floorNumber);
     Waiter(string name, int age, string position, int salary, int floorNumber, Order order);
     virtual ~Waiter();
     Waiter(const Waiter &other);
     Waiter(Waiter &&other) noexcept;
     Waiter& operator=(const Waiter &rhs);
 
+    void setFloorNumber(int floorNumber);
+
     int getFloorNumber() const;
+    Order getOrder() const;
 
+    static int getAmountOfWaiters();
 
+    void output(ostream &os) const override final;
+    void outputAmountOfEmployees() const override;
 };
 
 #endif //LAB2_CLASS_OBJECTS_WAITER_H
